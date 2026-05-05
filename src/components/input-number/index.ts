9cb18a1188c1a4cab452097ce3d36e2c44ad6bba
faizal <jai.crys@gmail.com>
2026-05-05 09:43:16 +0800

    feat(input-number): add KTInputNumber component with configuration and event handling interfaces

---
+++ src/components/input-number/index.ts
@@ -0,0 +1,11 @@
+/**
+ * KTUI - Free & Open-Source Tailwind UI Components by Keenthemes
+ * Copyright 2025 by Keenthemes Inc
+ */
+
+export { KTInputNumber } from './input-number';
+export type {
+	KTInputNumberConfigInterface,
+	KTInputNumberEventPayloadInterface,
+	KTInputNumberInterface,
+} from './types';
